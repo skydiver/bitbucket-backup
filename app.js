76f5c86aafe8616eccb2532181e4042d0ae0fd95
Sam Wrigley <sam@samwrigley.co.uk>
2016-10-31 16:04:45 +0000

    Added javascript string replace method to remove any whitespace in repo names and replace with hyphen

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ getAllRepos(url, auth, function (error, repos) {
 
 		// Choose between https and ssh authentication
 		var protocol = argv.opts.auth == 'ssh' ? 1 : 0;
-		
-		exec(command + ' clone ' + repo.links.clone[protocol].href + ' ' + backupFolder + repo.name, callback);
+
+		exec(command + ' clone ' + repo.links.clone[protocol].href + ' ' + backupFolder + repo.name.replace(/\s+/g, '-'), callback);
 	});
 });
 
