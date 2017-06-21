f8b8a4dd4245e7816677e60b312d3fe058a5fb44
Martin <skydiver@users.noreply.github.com>
2017-06-21 14:44:35 -0300

    moving to es6 + async / await

--- bitbucketbackup.js
+++ bitbucketbackup.js
@@ -1,6 +1,6 @@
 /**
  * Bitbucket backup script
- * Helps you backup all your bitbucket repositories to a local folder
+ * Helps you backup all your Bitbucket repositories to a local folder
  *
  * Usage: node app --user=bbUser --pass=bbPass --owner=companyOrUserName --folder=./backup --auth=ssh
  *
@@ -9,11 +9,8 @@
  */
 
 let path = require('path');
-let async = require('async');
 let argv = require('named-argv');
-let request = require('request');
-let exec = require('child_process').exec;
-let fs = require('fs');
+let repos = require('./repos');
 
 // Check the incoming params
 if (!argv.opts || !argv.opts.owner || !argv.opts.user || !argv.opts.pass) {
@@ -21,98 +18,15 @@ if (!argv.opts || !argv.opts.owner || !argv.opts.user || !argv.opts.pass) {
     process.exit(1);
 }
 
-let url = 'https://api.bitbucket.org/2.0/repositories/' + argv.opts.owner;
-let auth = {
+/* PARAMETERS */
+let params = {
+    url: 'https://api.bitbucket.org/2.0/repositories/' + argv.opts.owner,
+    protocol: argv.opts.auth == 'ssh' ? 1 : 0,
+    folder: path.normalize(argv.opts.folder || './bitbucket-repo-backups' + '/'),
     user: argv.opts.user,
     pass: argv.opts.pass
 };
 
-let backupFolder = argv.opts.folder || './bitbucket-repo-backups';
-backupFolder = path.normalize(backupFolder + '/');
-
-// Get all repos from Bitbucket
-getAllRepos(url, auth, function (error, repos) {
-    if (error) {
-        throw error;
-    }
-
-    console.log('Got %d repos. Processing...', repos.length);
-
-    // Iterate over all repos, clone each to local folder
-    async.eachLimit(repos, 500, function (repo, callback) {
-
-        // Remove any whitespace from repo name and make lowercase
-        let repoName = repo.name.replace(/\s+/g, '-').toLowerCase();
-
-        // Choose between git and mercurial
-        let command = repo.scm == 'git' ? 'git' : 'hg';
-
-        // Choose between https and ssh authentication
-        let protocol = argv.opts.auth == 'ssh' ? 1 : 0;
-
-        // If repo exists locally then fetch and pull
-        try {
-
-            if (fs.statSync(backupFolder + repoName)) {
-
-                console.log('Fetching and pulling...', repo.name);
-
-                exec('cd ' + backupFolder + repoName + ' && ' + command + ' fetch --all && ' + command + ' pull --all && cd ../..', callback);
-
-            }
-        }
-
-            // If repo does not exist locally then clone
-        catch (e) {
-
-            console.log('Cloning...', repo.name);
-
-            exec(command + ' clone ' + repo.links.clone[protocol].href + ' ' + backupFolder + repoName, callback);
-
-        }
-
-    });
-});
-
-/**
- * Get all repositories from the specified url
- *
- * @callback processCallback
- *
- * @param {String} url - Bitbucket API url
- * @param {Object} auth - Object containing bitbucket username and password
- * @param {processCallback} callback
- */
-function getAllRepos(url, auth, callback) {
-    let repos = [];
-    let apiUrl = url;
-
-    // Iterate bitbucket pages, collect all repositories to repos array
-    async.doWhilst(
-        function (callback) {
-            request.get(apiUrl, {auth: auth}, function (error, response, body) {
-                let json = null;
-
-                try {
-                    json = JSON.parse(body);
-                }
-                catch (exc) {
-                    return callback(exc);
-                }
-
-                repos = repos.concat(json.values);
-                apiUrl = json.next;
-                callback();
-            });
-        },
-
-        function () {
-            // Keep iterating until apiUrl (json.next) exists
-            return !!apiUrl;
-        },
-
-        function (error) {
-            callback(error, repos);
-        }
-    );
-}
+/* CLONE REPOS */
+repos.clone(params)
+    .catch(console.log);
---
+++ repos.js
@@ -0,0 +1,37 @@
+let rp = require('request-promise');
+let exec = require('child_process').exec;
+let fs = require('fs');
+
+exports.clone = async (params) => {
+
+    let {url, protocol, folder, user, pass} = params;
+
+    let response = await rp({method: 'GET', url, auth: {user, pass}});
+
+    let repos = JSON.parse(response).values;
+
+    for (let repo of repos) {
+
+        let repoName = repo.name.replace(/\s+/g, '-').toLowerCase();
+
+        let command = repo.scm == 'git' ? 'git' : 'hg';
+
+        try {
+            if (fs.statSync(folder + repoName)) {
+                console.log('Fetching and pulling:', repo.name);
+                exec('cd ' + folder + repoName + ' && ' + command + ' fetch --all && ' + command + ' pull --all && cd ../..', () => {
+                    console.log('Finished:', repo.name)
+                });
+            }
+        } catch (e) {
+            console.log('Cloning:', repo.name);
+            exec(command + ' clone ' + repo.links.clone[protocol].href + ' ' + folder + repoName, () => {
+                console.log('Finished:', repo.name)
+            });
+        }
+
+    }
+
+    return "";
+
+};
